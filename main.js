5578a72d54b389b6e633e7de40f22766817ce7a8
FioFathur7 <fiofathur@gmail.com>
2024-11-30 22:27:07 +0700

    update

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const hamBurger = document.getElementById("hamburger");
 const menuList = document.getElementById("menu");
+const body = document.getElementById('body');
 
 hamBurger.addEventListener('click', function () {
   menuList.classList.toggle('hidden')
   console.log("Menu Activated")
 });
-
